1253bab116925972784fbd213c7cc819e2d04e54
Javier González <jgestarriol@gmail.com>
2023-05-24 22:09:25 +0200

    Add types

---
+++ src/types/gotCharacters.tsx
@@ -0,0 +1,6 @@
+import { KingsHand } from "../models/kingsHand";
+import { Fighter } from "../models/fighter";
+import { King } from "../models/king";
+import { Squire } from "../models/squire";
+
+export type GotCharacters = King | Fighter | KingsHand | Squire;
